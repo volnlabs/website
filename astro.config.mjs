f422cfb8d26292c52d59541a690d78841f0b48a2
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2026-06-23 03:52:52 +0530

    feat: add SEO sitemap, robots, meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.volnlabs.com',
   output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
